a171ba18ab4fd222904e260869d0b2bcd4841d1e
Croissant-eng <ttrickyparchment@gmail.com>
2026-04-14 21:02:26 -0600

    Agregacion de subclases TareaSimple y TareaRecurrente, Cambio de Idioma a ingles en alguna documentacion

--- Classes/all_classes.py
+++ Classes/all_classes.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from datetime import datetime as dt
+from datetime import timedelta
 
 """
 Importamos los modulos enum y date porque:
@@ -20,7 +21,8 @@ class Prioridad(Enum):
     """
     El usuario puede ingresar 
     como parametro de Prioridad ya sea
-    (1, 2, ...) ó Estado.(Baja, Media, ...)"""
+    (1, 2, ...) ó Estado.(Baja, Media, ...)
+    """
 
 class Estado(Enum):
     PENDIENTE = 'pendiente'
@@ -34,12 +36,13 @@ class Frecuencia(Enum):
 
 # Calse base Tarea
 class Tarea:
-    def __init__(self, titulo: str, descripcion: str, fecha_lim: dt, prioridad: Prioridad):
+    def __init__(self, titulo: str, descripcion: str, 
+                 fecha_lim: dt, prioridad: Prioridad):
         self.titulo = titulo
         self.descripcion = descripcion
         self.fecha_lim = fecha_lim
         self._prioridad = prioridad
-        self._estado = Estado.PENDIENTE    #
+        self._estado = Estado.PENDIENTE    # En la creacion del objeto ya se inicializa su estado
         self.fecha_creacion = dt.now()     # Nos da la fecha acutal en formato año-mes-dia
     
     @property
@@ -51,29 +54,67 @@ class Tarea:
         return self._prioridad
 
     def completar(self):
-        """Marca una tarea como completada"""
+        """Changes the status of a homework/task to COMPLETE"""
         self._estado = Estado.COMPLETADA
     
     def reabrir(self):
-        """Reabre una tarea completada"""
+        """Reopens a taks/homework"""
         if self._estado == Estado.COMPLETADA:
             self._estado = Estado.PENDIENTE
         else:
             raise ValueError('You can reopen a task that´s already open')
 
     def dias_restantes(self):
-        """Calcula los dias restantes hasta la fecha limite"""
+        """Counts how many days the homework/task has left"""
         hoy = dt.now()
         delta = self.fecha_lim - hoy
         return delta.days             # Devuelve un numero entero
     
     def esta_vencida(self):
-        """Verifica si la tarea esta vencida"""
+        """
+        Checks if the homework/task 
+        its expired and returns True or False
+        rather it is or not"""
         return (dt.now() > self.fecha_lim) and (self._estado != Estado.COMPLETADA)
     
     def __repr__(self):
+        """Repr method of the class"""
         cadena = f'La tarea: {self.titulo}\n'
         cadena += f'Su estado es: {self._estado}'
         cadena += f'Tiene como Prioridad: {self._prioridad.name}\n'
         cadena += f'Y su fecha limite es: {self.fecha_lim}'
         return cadena
+
+
+class TareaSimple(Tarea):
+    
+
+    def __init__(self, titulo : str, descripcion : str, 
+                 prioridad : Prioridad, fecha_lim : dt):
+        super.__init(titulo, descripcion, prioridad, fecha_lim)
+    
+    def completar(self):
+        super().completar()
+        print(f'Tarea Simple {self.titulo} ha sido completada')
+    
+class TareaRecurrente(Tarea):
+
+
+    def __init__(self, titulo: str, descripcion: str,
+                 prioridad: Prioridad, fecha_lim: dt,
+                 frecuencia : Frecuencia):
+        super.__init__(titulo, descripcion, prioridad, fecha_lim)
+        self.frecuencia = frecuencia
+    
+    def completar(self):
+        """
+        In this subclass what´s fuction does different 
+        from its mother class or the other subclass 
+        its that it restarts the homework/task status 
+        back to PENDIENTE and its fecha_lim gets moved forward
+        by the number of days the user put.
+        """
+        super().completar()
+        self.ultima_completada = dt.now()
+        self.fecha_lim = self.fecha_lim + timedelta(days=self.frecuencia.value)
+        self.estado = Estado.PENDIENTE
